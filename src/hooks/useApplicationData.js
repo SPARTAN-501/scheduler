53c9018c8d301e2f3bb0a23491162a1270f190d9
Luke Yin <luke.yin@alumni.ubc.ca>
2021-03-29 19:41:31 -0700

    Removed unused code

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,7 +10,6 @@ export default function useApplicationData() {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers"),
     ]).then((all) => {
-      const [first, second, third] = all;
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, [])
